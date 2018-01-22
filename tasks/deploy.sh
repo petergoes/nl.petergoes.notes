c925c5385a5cc0828978c0a057214a360bd4d0da
Peter Goes <petergoes@gmail.com>
2018-01-22 22:11:04 +0100

    blub

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -27,8 +27,6 @@ git rm -r *
 # copy stored files
 cp -Rf $HOME/build/* .
 
-git rm --cached petergoes/notes
-
 echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
 echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
 
